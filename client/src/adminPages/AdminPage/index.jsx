d9870f394c1bb0761fb93cbf638f27eb8a55d029
Aviba <avibarashi315248@gmail.com>
2025-08-06 13:45:27 +0300

    refactor: Simplify admin log schema by removing unused fields and updating action enum

--- client/src/adminPages/AdminPage/index.jsx
+++ client/src/adminPages/AdminPage/index.jsx
@@ -3,11 +3,23 @@ import { Link, useNavigate } from 'react-router-dom'
 import './style.scss'
 
 export default function AdminPage() {
-  const navigate = useNavigate()
 
   return (
     <div className="about-page">
-    
+    <div className='logs'></div>
+    <div className='actions'>
+      <div className='recpiesWatingForApproval'></div>
+      <div className='allRecepies' > 
+        
+        <table>
+          <th>יוזר</th>
+          <th>שם מתכון</th>
+          <th>קישור למתכון</th>
+          <th>סטטוס</th>
+          </table>
+           </div>
+      <div ></div>
+    </div>
     </div>
   )
 }
