98f7649c913e83a10a977553474d066878b78218
Aviba <avibarashi315248@gmail.com>
2025-08-06 12:46:12 +0300

    feat: Add admin login and admin panel pages; implement validation and styling for login form

---
+++ client/src/adminPages/AdminPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import './style.scss'
+
+export default function AdminPage() {
+  const navigate = useNavigate()
+
+  return (
+    <div className="about-page">
+    
+    </div>
+  )
+}
